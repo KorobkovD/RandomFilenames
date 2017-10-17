84c71d527b610cd94c026cc4ca10e830a2b51eb6
Korobkov Denis <den-kor.int@mail.ru>
2017-10-17 15:04:55 +0300

    Добавьте файлы проекта.

---
+++ RandomFilenames/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace RandomFilenames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonOFD = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбранная директория:";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 25);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(237, 20);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(114, 51);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(166, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Переименовать все файлы";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonOFD
+            // 
+            this.buttonOFD.Location = new System.Drawing.Point(255, 24);
+            this.buttonOFD.Name = "buttonOFD";
+            this.buttonOFD.Size = new System.Drawing.Size(25, 22);
+            this.buttonOFD.TabIndex = 3;
+            this.buttonOFD.Text = "...";
+            this.buttonOFD.UseVisualStyleBackColor = true;
+            this.buttonOFD.Click += new System.EventHandler(this.buttonOFD_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(12, 56);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(35, 13);
+            this.labelInfo.TabIndex = 4;
+            this.labelInfo.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 80);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonOFD);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonOFD;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ RandomFilenames/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RandomFilenames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelInfo.Text = "";
+        }
+
+        private void buttonOFD_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Выбор папки с файлами";
+            if (textBoxPath.Text.Length > 0)
+                fbd.SelectedPath = textBoxPath.Text;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (textBoxPath.Text.Length == 0)
+            {
+                MessageBox.Show("Не указана папка для переименования файлов");
+            }
+            else
+            {
+                labelInfo.Text = "";
+                string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                Random rndGenerator = new Random();
+                try
+                {
+                string path = textBoxPath.Text;
+                    FileInfo[] files = new DirectoryInfo(path).GetFiles();
+                    foreach (FileInfo fi in files)
+                    {
+                        string fullName = fi.FullName;
+                        string name = fi.Name;
+                        string extension = fi.Extension;
+                        char[] rndFilename = new char[6];
+                        for (int i = 0; i < rndFilename.Length; i++)
+                            rndFilename[i] = alph[rndGenerator.Next(alph.Length)];
+                        string stringFilename = new string(rndFilename);
+                        File.Move(fullName, Path.Combine(path, stringFilename + extension));
+                    }
+                    labelInfo.Text = "Готово!";
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+        }
+    }
+}
